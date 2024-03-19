1c9faf4e25b5a25b8b89256a915f14f1cd781a31
aluno <aluno@fateclins.local>
2024-03-19 11:10:14 -0300

    durante a facul

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
 import { useState } from 'react'
 import './App.css'
+import { clean, inserirNumero, operation, resultado } from './functions/functions';
 
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState('');
 
-  // const handleClick = (e) = => {
-
-  // }
+  const onChange = (event) => {
+    setResult(event.target.value);
+  };
 
   return (
     <>
-      <h1>Calculadora em React</h1>
-
       <div className="container">
-        <form>
-          <input type="text" value={result}/>
+      <h1 className='text-slate-300'>Calculadora em React</h1>
+        <form className=' my-5 py-2'>
+          <input type="text" value={result} className='border-2 border-black-600 rounded-lg w-full py-3'/>
         </form>
 
-        <div className="keypad grid grid-cols-4">
-          <button>Clear</button>
-          <button>C</button>
-          <button>&divide;</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>&times;</button>
-          <button>6</button>
-          <button>5</button>
-          <button>4</button>
-          <button>&ndash;</button>
-          <button>3</button>
-          <button>2</button>
-          <button>1</button>
-          <button>+</button>
-          <button></button>
-          <button></button>
-          <button></button>
+        <div className="keypad grid grid-cols-4 gap-2">
+          <button className='bg-slate-300'>Clear</button>
+          <button onClick={clean()} className='bg-slate-300'>C</button>
+          <button onClick={resultado()} className='bg-slate-300'></button>
+          <button onClick={operation('/')} className='bg-slate-300'>&divide;</button>
+          <button onClick={inserirNumero('7')} className='bg-slate-300'>7</button>
+          <button onClick={inserirNumero('8')} className='bg-slate-300'>8</button>
+          <button onClick={inserirNumero('9')} className='bg-slate-300'>9</button>
+          <button onClick={operation('*')} className='bg-slate-300'>&times;</button>
+          <button onClick={inserirNumero('6')} className='bg-slate-300'>6</button>
+          <button onClick={inserirNumero('5')} className='bg-slate-300'>5</button>
+          <button onClick={inserirNumero('4')} className='bg-slate-300'>4</button>
+          <button onClick={operation('-')} className='bg-slate-300'>&ndash;</button>
+          <button onClick={inserirNumero('1')} className='bg-slate-300'>1</button>
+          <button onClick={inserirNumero('2')} className='bg-slate-300'>2</button>
+          <button onClick={inserirNumero('3')} className='bg-slate-300'>3</button>
+          <button onClick={operation('+')} className='bg-slate-300'>+</button>
+          <button className='bg-slate-300'>.</button>
+          <button onClick={inserirNumero('0')} className='bg-slate-300'>0</button>
+          <button onClick={resultado()} className='bg-blue-500'>=</button>
         </div>
-      </div>
-      
+      </div>     
     </>
   )
 }
---
+++ src/functions/functions.tsx
@@ -0,0 +1,19 @@
+var valor1 = 0;
+var valor2 = 0;
+
+
+export function inserirNumero(){
+    document.getElementById.value
+}
+
+export function operation(){
+
+}
+
+export function resultado(){
+
+}
+
+export function clean(){
+  
+}
